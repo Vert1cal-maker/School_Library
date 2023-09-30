5338d2ef8f6732e4b2357df6021826840a2fbdfe
Vert1cal-maker <128141917+Vert1cal-maker@users.noreply.github.com>
2023-09-30 15:04:30 +0300

    Update create_books_authors.sql

--- src/sql/migration/create_books_authors.sql
+++ src/sql/migration/create_books_authors.sql
@@ -1,5 +1,7 @@
 create table if not exists Books_Authors (
 book_id int not null,
 author_id int not null,
+FOREIGN KEY (book_id) REFERENCES Books (id),
+FOREIGN KEY (author_id) REFERENCES Authors (id),
 primary key (book_id, author_id)
 );
